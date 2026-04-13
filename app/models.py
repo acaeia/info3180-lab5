c981361b10012e93971341386cd0c019b64c7417
Alexia Barrows <alexiabarrows@gmail.com>
2026-04-13 18:09:24 -0500

    Created Movie Class

--- app/models.py
+++ app/models.py
@@ -1,1 +1,30 @@
-# Add any model classes for Flask-SQLAlchemy here
+from app import db
+from datetime import datetime
+from flask import url_for
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    poster = db.Column(db.String(200), nullable=False)  
+    created_at = db.Column(db.DateTime, nullable=False)
+    
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'poster': url_for('getImage', filename = self.poster),
+            'created_at': self.created_at
+        }
+        
+    def __repr__(self):
+        return f'<Movie {self.title}>'
